c86c49b04399d168cba93ec7f5fbe752a25022ec
JacopoDaeli <jacopo.daeli@gmail.com>
2016-07-07 15:38:45 +0200

    Fix config file

--- app/config/index.js
+++ app/config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     pageAccessToken: process.env.MESSENGER_PAGE_ACCESS_TOKEN
   },
   botengine: {
-    botUrl: process.env.BOTENGINE_BOT_URL || 'http://127.0.0.1:3978'
+    botUrl: process.env.BOTENGINE_BOT_URL || 'http://127.0.0.1:3978',
     endpoints: {
       messages: '/api/v1/messages'
     },
